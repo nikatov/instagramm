2fb9f767dad93eea5e66d7d963f9a580bd652b95
Владислав Никатов <nikatov@ivk.ru>
2021-06-06 21:43:01 +0300

    переключение на избранное, нижняя навигацияя

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,21 +1,23 @@
+import React from 'react'
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { Platform } from 'react-native';
 import { MainScreen } from '../screens/MainScreen';
 import { PostScreen } from '../screens/PostScreen';
+import { BookedScreen } from '../screens/BookedScreen';
 import { THEME } from '../theme';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 const PostNavigator = createStackNavigator(
-  // Навигация
-  {
+  { // Навигация
     Main: MainScreen,
     Post: {
       screen: PostScreen
     },
   },
-  // Конфиг
-  {
-    initialRouteName: 'Main', // экран, который открывается первым (по-умолчанию, первый в навигации)
+  { // Конфиг
+    // initialRouteName: 'Main', // экран открывается первым по-умолчанию, тк первый в навигации
     defaultNavigationOptions: {
       headerStyle: {
         backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff',
@@ -24,4 +26,44 @@ const PostNavigator = createStackNavigator(
     },
   });
 
-export const AppNavigation = createAppContainer(PostNavigator);
+// Тк страницы списка постов и списка избранных постов идентичны, имеются лишь разные данные,
+// то Навигатор необходим чтобы избежать лишнего перерендера лишь изменяя состояние страницы,
+// а не открывая новую.
+const BookedNavigator = createStackNavigator(
+  { // Навигация
+    Booked: BookedScreen,
+    Post: PostScreen
+  },
+  { // Конфиг
+    // initialRouteName: 'Booked', // экран открывается первым по-умолчанию, тк первый в навигации
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff',
+      },
+      headerTintColor: Platform.OS === 'android' ? '#fff' : THEME.MAIN_COLOR,
+    },
+  });
+
+// Объединение двух навигаторов
+const BottomNavigator = createBottomTabNavigator(
+  { // Навигация
+  Post: { // Страница по-умолчанию, поэтому первая, чтобы не писать в конфиге 'initialRouteName: Post'
+    screen: PostNavigator, // передача навигатора
+    navigationOptions: {
+      tabBarIcon: info => <Ionicons name='ios-albums' size={25} color={info.tintColor} /> // иконка, соотстветствующая скрину
+    }
+  },
+  Booked: {
+    screen: BookedNavigator, // передача навигатора
+    navigationOptions: {
+      tabBarIcon: info => <Ionicons name='ios-star' size={25} color={info.tintColor} /> // иконка, соотстветствующая скрину
+    }
+  }
+},
+{ // Конфиг
+  tabBarOptions: {
+    activeTintColor: THEME.MAIN_COLOR // Цвет иконок и текста под ними
+  }
+})
+
+export const AppNavigation = createAppContainer(BottomNavigator);
