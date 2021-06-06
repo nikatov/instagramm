62e26439382c055c46e302d599fa93b893090cf4
Владислав Никатов <nikatov@ivk.ru>
2021-06-06 15:48:02 +0300

    стилизация навигации

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,7 +1,9 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { Platform } from 'react-native';
 import { MainScreen } from '../screens/MainScreen';
 import { PostScreen } from '../screens/PostScreen';
+import { THEME } from '../theme';
 
 const PostNavigator = createStackNavigator(
   // Навигация
@@ -13,7 +15,13 @@ const PostNavigator = createStackNavigator(
   },
   // Конфиг
   {
-    initialRouteName: 'Main' // экран, который открывается первым (по-умолчанию, первый в навигации)
+    initialRouteName: 'Main', // экран, который открывается первым (по-умолчанию, первый в навигации)
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff',
+      },
+      headerTintColor: Platform.OS === 'android' ? '#fff' : THEME.MAIN_COLOR,
+    },
   });
 
 export const AppNavigation = createAppContainer(PostNavigator);
