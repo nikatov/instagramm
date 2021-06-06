ea53d1d14f90d701fd62b19efa9b9c091439d0e7
Владислав Никатов <nikatov@ivk.ru>
2021-06-06 20:39:21 +0300

    Добавлен экран поста

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,12 +1,44 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet, Button, ScrollView, Alert } from 'react-native';
+import { DATA } from '../data';
+import { THEME } from '../theme';
 
 export const PostScreen = ({ navigation }) => {
   const postId = navigation.getParam('postId');
+  const post = DATA.find(el => el.id === postId);
+
+  const removeHandler = () => (
+    Alert.alert(
+      'Удаление поста',
+      'Вы уверены, что хотите удалить пост?',
+      [
+        {
+          text: 'Отмена',
+          onPress: () => console.log('Отменено удаление'),
+          style: 'cancel'
+        },
+        {
+          text: 'Удалить',
+          onPress: () => console.log('Удаление поста'),
+          style: 'danger'
+        }
+      ],
+      { cancelable: false } // чтобы нельзя было закрыть окно по бекграунду
+    )
+  )
+
   return (
-    <View style={styles.main}>
-      <Text>{postId}</Text>
-    </View>
+    <ScrollView style={styles.main}>
+      <Image style={styles.image} source={{uri: post.img}} />
+      <View style={styles.textWrap}>
+        <Text style={styles.text}>{post.text}</Text>
+      </View>
+      <Button
+        title='Удалить'
+        color={THEME.DANGER_COLOR}
+        onPress={removeHandler}
+      />
+    </ScrollView>
   );
 }
 
@@ -20,7 +52,15 @@ PostScreen.navigationOptions = ({ navigation }) => {
 const styles = StyleSheet.create({
   main: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  },
+  image: {
+    width: '100%',
+    height: 200
+  },
+  textWrap: {
+    padding: 10
+  },
+  text: {
+    fontFamily: 'open-regular'
   }
 })
